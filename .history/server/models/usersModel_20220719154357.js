84f35487d729cdbcfa1914001ca47d23a54b08d8
InternalShadow <76181682+InternalShadow@users.noreply.github.com>
2022-07-19 15:50:55 -0400

    Windows webpack

---
+++ .history/server/models/usersModel_20220719154357.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = "mongodb+srv://heatedChickens:Apple123@hike-app.bpa7q.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(MONGO_URI, {
+  // options for the connect method to parse the URI
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // sets the name of the DB that our collections are part of
+  dbName: 'hikeTracker'
+})
+.then(() => console.log('Connected to Mongo DB.'))
+.catch(err => console.log(err));
+  
+
+const UserSchema = new mongoose.Schema({
+  firstName : { type : String, required : true } ,
+  lastName : { type : String, required : true }, 
+  location : { type : String, required : true },
+  email : { type : String, required : true },
+  password : { type : String, required : true }
+});
+
+const User = mongoose.model('users', UserSchema);
+
+
+module.exports = User;
