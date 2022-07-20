d7ec3f66f828b8b4661e72222856305043c3c19d
lnhunt11 <86940809+lnhunt11@users.noreply.github.com>
2022-07-19 17:13:07 -0700

    favoriteSchema complete

---
+++ server/models/favoriteModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+
+const FavoriteSchema = new mongoose.Schema({
+  title: {type: String, required: false},
+  shortDescription: {type: String, required : false},
+  state: { type : String, required : true}, 
+  duration: { type : String, required : false},
+  latitude: {type: String, required: false},
+  longitude: {type: String, required: false},
+  location: {type: String, required: false}
+});
+
+const Favorite = mongoose.model('favorites', FavoriteSchema)
+
+module.exports = Favorite;
