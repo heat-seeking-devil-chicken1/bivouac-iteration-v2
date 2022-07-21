011af0f165905e509be97918eefb39e9b3fca434
Darin <dngau94@gmail.com>
2022-07-21 16:49:20 -0400

    successfully sent google data to frontend, but where???

--- server/routes/googleRoute.js
+++ server/routes/googleRoute.js
@@ -19,24 +19,38 @@ passport.use(new GoogleStrategy(
   function(accessToken, refreshToken, profile, done) {
     console.log('wow in callback function');
     console.log(profile.id);
-    User.findOrCreate({googleId: profile.id, firstName: profile.given_name, lastName: profile.family_name, email: profile.email}, function(err, User){
-      console.log(User);
-      return done(err, User);
-    })
-
-    /* 
-      Google OAuth works! The logged profile has user data from Google. In this function we need to either create a new user or find the existing user based on the given data received in profile from Google {firstName : givenName}
-    */
-   console.log();
+    User.findOrCreate({googleId: profile.id, firstName: profile.given_name, lastName: profile.family_name, email: profile.email}, function(err, user){
+      console.log('in the findOrCreate');
+      // res.locals.googleUser = googleUser;
+      console.log('finishing the findOrCreate')
+      return done(err, user);
+    });
   }
 ));
+
+//serialize
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+//deserialize is used to end login session.
+passport.deserializeUser((user, done) => {
+  User.findById(user.id).then(id => {
+    done(null, id);
+  })
+});
+
+
 // auth with google
 router.get('/google', passport.authenticate('google', { scope:
   ['profile', 'email']
 }));
 
 // callback route for google to redirect to
-router.get('/google/redirect', passport.authenticate('google', {failureRedirect: '/login'}), (req, res) => res.redirect('/'));
+router.get('/google/redirect', passport.authenticate('google', {failureRedirect: '/login'}), (req, res) => {
+  const googleData = req.session.passport.user;
+  res.status(200).send(googleData); // where do you get sent to ? local storage? 
+});
 
 
 module.exports = router;
