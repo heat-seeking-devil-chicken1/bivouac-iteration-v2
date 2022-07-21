64b7b34c6a349f44b32f5bdea39dd8c2e337c462
Darin <dngau94@gmail.com>
2022-07-20 21:18:08 -0400

    fixed google login redirect. This current commit works perfectly fine

--- server/routes/googleRoute.js
+++ server/routes/googleRoute.js
@@ -1,65 +1,29 @@
-// // similiar to 'routs/auth.js'
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oidc');
+const session = require('express-session');
 
-// const express = require('express');
-// const router = express.Router();
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oidc');
-// //const db = require('../db'); // not sure, might throw error. This file is db.js that has not been created yet
+require('dotenv').config();
 
-// passport.use(new GoogleStrategy({
-//   clientID: process.env['GOOGLE_CLIENT_ID'], //potential break?
-//   clientSecret: process.env['GOOGLE_CLIENT_SECRET'], //potential break;
-//   callbackURL: '/oauth2/redirect/google',
-//   scope: [ 'profile' ]
-// }, function verify(issuer, profile, cb) {
-//   db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-//     issuer,
-//     profile.id
-//   ], function(err, row) {
-//     if (err) { return cb(err); }
-//     if (!row) {
-//       db.run('INSERT INTO users (name) VALUES (?)', [
-//         profile.displayName
-//       ], function(err) {
-//         if (err) { return cb(err); }
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:8080"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+  }
+));
 
-//         var id = this.lastID;
-//         db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-//           id,
-//           issuer,
-//           profile.id
-//         ], function(err) {
-//           if (err) { return cb(err); }
-//           var user = {
-//             id: id,
-//             name: profile.displayName
-//           };
-//           return cb(null, user);
-//         });
-//       });
-//     } else {
-//       db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
-//         if (err) { return cb(err); }
-//         if (!row) { return cb(null, false); }
-//         return cb(null, row);
-//       });
-//     }
-//   });
-// }));
-
-
-// router.get('/glogin', function(req, res, next) {
-//   res.render('login');
-// });
-
-// router.get('/login/federated/google', passport.authenticate('google'));
+router.get('/federated/google', passport.authenticate('google'));
 
 // router.get('/oauth2/redirect/google', passport.authenticate('google', {
 //   successRedirect: '/',
 //   failureRedirect: '/login'
 // }));
 
-
-// module.exports = router;
-
-// ///auth.js 
+module.exports = router;
